b3718e77a7b0af344f16fe318cbb5a102fc1d7e9
Samuel French <samfrench@gmail.com>
2015-02-20 00:23:58 -0600

    exchange rate images and styling

--- src/main/webapp/resources/js/widget/exchangeRate.js
+++ src/main/webapp/resources/js/widget/exchangeRate.js
@@ -5,10 +5,21 @@ $(document).ready(function(){
 
 function getExchangeRate(){
 	$.get("/french_ventures_secure/misc/exchangeRate", function(data){
-		$('#exchangeRate').html(data.usdrate + "USD/" + data.cnyrate + "CNY");
+		$('#exchangeRate').html("Current Exchange Rate: <br />" + data.usdrate + 
+				"USD<img class='widget_flag widget_flag_usa' src='/french_ventures_secure/resources/image/flag/USA_min.png' /> / " + 
+				data.cnyrate + "CNY <img class='widget_flag widget_flag_prc' src='/french_ventures_secure/resources/image/flag/PRC_min.png' />" +
+						"<br /> (Updated hourly - See legal for full disclaimer)");
+		resizeWidgetFlag('widget_flag_usa');
+		resizeWidgetFlag('widget_flag_prc');
 	}).fail(function(){
 		$('#exchangeRate').html("Try again later.");
 	}).always(function(){
 		console.log("info: exchange rate loaded.");
 	});
+}
+
+function resizeWidgetFlag(imgClass)
+{
+	$('.' + imgClass).css('width','18px');
+	$('.' + imgClass).css('height','12px');
 }
